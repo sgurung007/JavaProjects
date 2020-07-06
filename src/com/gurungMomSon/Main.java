c03295ad665eabaaf3993218486b107f2c421299
surajGurung <sgrg007@gmail.com>
2020-07-06 00:51:37 -0500

    properties file is added and implemented in main class

--- src/com/gurungMomSon/Main.java
+++ src/com/gurungMomSon/Main.java
@@ -1,12 +1,30 @@
 package com.gurungMomSon;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 public class Main {
 
     private static MyAbstract person1;
+    private static Properties configuration1;
+    private static FileInputStream configurationFile;
 
-    public static void main(String[] args) {
-	person1=new Employee();
-	person1.displayEmail();
+    public static void main(String[] args) throws IOException {
+        person1 = new Employee();
+        person1.displayEmail();
+
+        configuration1 = new Properties();
+        configurationFile = new FileInputStream("config/configuration.properties");
+        configuration1.load(configurationFile);
+        System.out.println(configuration1.getProperty("url")+"\n"+
+                configuration1.getProperty("userName")+"\n"+
+                configuration1.getProperty("passWord")+"\n"+
+                configuration1.getProperty("isEligible")+"\n"+
+                configuration1.getProperty("salary")+"\n"+
+                System.getProperty("user.dir")
+                );
+
 
     }
 }
